ea0543bb31cb7f67871adce72b1bf790636759b5
yoosumi <hugbee@naver.com>
2023-05-08 17:17:42 +0900

    feat: 현재 위치 가져오기

--- app/src/main/java/com/example/nalssieottae/MainActivity.kt
+++ app/src/main/java/com/example/nalssieottae/MainActivity.kt
@@ -1,22 +1,81 @@
 package com.example.nalssieottae
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.databinding.DataBindingUtil
 import com.example.nalssieottae.databinding.ActivityMainBinding
+import com.google.android.gms.location.LocationServices
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        binding.recyclerViewMainWeather.adapter = WeatherListAdapter(listOf(WeatherItem("오전 7시", R.drawable.ic_cloud, "-4"),
-            WeatherItem("오전 9시", R.drawable.ic_sunny, "0")))
+        // 데이터바인딩
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.lifecycleOwner = this
 
-        binding.recyclerViewMainWeekWeather.adapter = WeekWeatherListAdapter(listOf(WeatherItem("오전 7시", R.drawable.ic_cloud, "-4"),
-            WeatherItem("오전 9시", R.drawable.ic_sunny, "0")))
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    // Precise location access granted.
+                    Log.d("getWeatherTest", "위치 권한 있음.")
+                    getCurrentLocation()
+                    // 그냥 진행
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    // Only approximate location access granted.
+                    Log.d("getWeatherTest", "Only approximate location access granted.")
+
+                }
+                else -> {
+                    Log.d("getWeatherTest", "위치 권한을 부여해주세요.")
+                    // 앱 강제 종료
+                }
+                // No location access granted.
+            }
+        }
+
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
     }
+
+    private fun getCurrentLocation(): Boolean {
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.d("getWeatherTest", "위치 권한 없음")
+
+            return false
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location ->
+                // Got last known location. In some rare situations this can be null.
+                Log.d("getWeatherTest", "${location.latitude}") // 위도: 37.4835657
+                Log.d("getWeatherTest", "${location.longitude}") // 경도: 127.0190708
+                // 위치 얻었고 이걸로 날씨
+            }
+        return true
+    }
+
 }
